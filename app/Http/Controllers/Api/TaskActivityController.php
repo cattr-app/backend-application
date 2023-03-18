23ec22f04a37fe857d4484bcb9fc6441a524876a
Ацамаз В <aczamaz@gmail.com>
2023-03-18 16:03:47 +0300

    add policy for action and refactoring

--- app/Http/Controllers/Api/TaskActivityController.php
+++ app/Http/Controllers/Api/TaskActivityController.php
@@ -2,48 +2,48 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Requests\Status\DestroyStatusRequest;
-use App\Http\Requests\TaskComment\CreateTaskCommentRequest;
-use App\Http\Requests\TaskComment\DestroyTaskCommentRequest;
-use App\Http\Requests\TaskComment\ListTaskCommentRequest;
-use App\Http\Requests\TaskComment\ShowTaskCommentRequestStatus;
-use App\Http\Requests\TaskComment\UpdateTaskCommentRequest;
-use Filter;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\TaskActivity\ShowTaskActivityRequest;
 use App\Models\TaskComment;
 use App\Models\TaskHistory;
-use Exception;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Throwable;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Auth;
 
-class TaskActivityController
+class TaskActivityController extends Controller
 {
     protected $taskComment;
     protected $taskHistory;
 
-    public function __construct(TaskComment $taskComment,TaskHistory $taskHistory)
+    public function __construct(TaskComment $taskComment, TaskHistory $taskHistory)
     {
         $this->taskComment = $taskComment;
         $this->taskHistory = $taskHistory;
     }
-    public function getCollection(string $type,int $taskId) {
+
+    public function getCollection(string $type,int $taskId): array
+    {
         $comments = $this->taskComment->where('task_id', $taskId)->with('user')->get()->toArray();
         $history = $this->taskHistory->where('task_id', $taskId)->with('user')->get()->toArray();
-
+        $result = [];
+        
         if( $type === "all" ) {
-            return array_merge($comments, $history);
+            $result = array_merge($comments, $history);
         } else if ( $type === "history" ) {
-            return $history;
+            $result = $history;
         } else if ($type === "comments") {
-            return $comments;
+            $result = $comments;
         }
+
+        foreach ($result as &$item) {
+            $item['can_change'] = $item['user_id'] === Auth::id();
+        }
+        return $result;
     }
-    public function sortCollection(array &$collection, string $sort)
+
+    public function sortCollection(array &$collection, string $sort): void
     {
         usort($collection, function ($a, $b) use ($sort) {
             if ($sort === "desc")
@@ -52,22 +52,24 @@ class TaskActivityController
                 return strtotime($a['created_at']) > strtotime($b['created_at']);
         });
     }
-    public function index(Request $request)
-    {
-        $currentPage = $request->get('page');
 
+    public function getPaginateCollection(array $collection, int $currentPage, $perPage = 10): LengthAwarePaginator
+    {
+        $collection = new Collection($collection);
+
+        $currentPageSearchResults = $collection->slice(($currentPage - 1) * $perPage, $perPage)->all();
+
+        return new LengthAwarePaginator($currentPageSearchResults, count($collection), $perPage);
+    }
+
+    public function index(ShowTaskActivityRequest $request): JsonResponse
+    {
         $searchResults = $this->getCollection($request->get('type'), $request->get('task_id'));
 
         $this->sortCollection($searchResults, $request->get('sort'));
 
-        $collection = new Collection($searchResults);
+        $paginatedSearchResults = $this->getPaginateCollection($searchResults, $request->get('page'));
 
-        $perPage = 10;
-
-        $currentPageSearchResults = $collection->slice(($currentPage - 1) * $perPage, $perPage)->all();
-
-        $paginatedSearchResults = new LengthAwarePaginator($currentPageSearchResults, count($collection), $perPage);
-        
         return responder()->success($paginatedSearchResults)->respond();
     }
 }
